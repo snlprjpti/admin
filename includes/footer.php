d1d6b3e192d1ee0d4148f7ab7903f6597a2bb991
YM-Srijan <srijan.karki@youngminds.com.np>
2017-07-19 17:15:28 +0545

    das

---
+++ includes/footer.php
@@ -0,0 +1,36 @@
+     <footer class="main-footer">
+        <!-- To the right -->
+        <div class="pull-right hidden-xs">
+          Anything you want
+        </div>
+        <!-- Default to the left -->
+        <strong>Copyright &copy; 2015 <a href="#">Company</a>.</strong> All rights reserved.
+      </footer>
+
+      <!-- Control Sidebar -->
+      <aside class="control-sidebar control-sidebar-dark">
+         <ul class="nav nav-tabs nav-justified control-sidebar-tabs">
+         <h2>Configuration</h2>
+        </ul>
+        <!-- Tab panes -->
+        <div class="tab-content">
+          <!-- Home tab content -->
+          <div class="tab-pane" id="control-sidebar-home-tab">
+            
+          </div><!-- /.tab-pane -->
+        </div>
+      </aside><!-- /.control-sidebar -->
+      <!-- Add the sidebar's background. This div must be placed
+           immediately after the control sidebar -->
+      <div class="control-sidebar-bg"></div>
+    </div><!-- ./wrapper -->
+`
+    <script src="./js/jQuery-2.1.4.min.js"></script>
+    <!-- Bootstrap 3.3.5 -->
+    <script src="./js/bootstrap.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="./js/app.min.js"></script>
+        <script src="./js/demo.js"></script>
+
+  </body>
+</html>
